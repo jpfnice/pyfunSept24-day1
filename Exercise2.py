932bd1f4c1d722984c2e1966c56b0c98bd7952aa
jpfnice <jpfnice@gmail.com>
2024-09-10 16:40:37 +0200

    Add files via upload

--- Exercise2.py
+++ Exercise2.py
@@ -22,3 +22,75 @@ Example:
     the truncated mean will only take into account the elements:
     [4,6,55,45,45] (3 and 77 are ignored)
 """
+
+numbers=[]   # Creation of an empty list <=> numbers=list()
+
+# Step 1:
+while True:
+    answer=input("Enter an int or 'stop': ")
+    if answer=='stop':
+        break
+    else:
+        answer=int(answer)
+        numbers.append(answer)
+    
+        # or 
+        # numbers.append(int(answer))
+     
+        
+# Step 2:
+print(f"Here is the constructed list: {numbers} it's size is {len(numbers)}")
+# or
+print("Here is the constructed list:",numbers, "it's size is", len(numbers))
+
+# Step 3:
+if len(numbers) > 0: # If the list numbers is not empty
+    
+    mini=numbers[0] 
+    maxi=numbers[0] 
+    total=numbers[0] 
+    # or
+    # mini=maxi=total=numbers[0]
+    
+    for e in numbers[1:]: 
+        # e = 5
+        if e < mini:
+            mini=e
+        if e > maxi:
+            maxi=e
+        total = total + e
+        
+    print(f"Minimum: {mini} Maximum: {maxi}")    
+    print(f"Mean: {total/len(numbers)}")
+else:
+    print("The list is empty")    
+    
+# Another way to obtain the same result but with the predefined functions:
+# min(), max(), sum()
+    
+if len(numbers) > 0:
+    print(f"Minimum: {min(numbers)} Maximum: {max(numbers)}")    
+    print(f"Mean: {sum(numbers)/len(numbers)}")
+    # or
+    import statistics
+    print(f"Mean: {statistics.mean(numbers)}")
+    
+else:
+    print("The list is empty")
+
+# Step 4: computation of the "truncated means"
+
+if len(numbers) > 0:
+    
+    org=numbers.copy() 
+    numbers.sort() # "in place"
+  
+    miniCount=numbers.count(numbers[0]) 
+    maxiCount=numbers.count(numbers[-1]) 
+    print(numbers)
+    subList=numbers[miniCount:-maxiCount]  
+    print(subList)
+    if len(subList) != 0:
+        print(f"Truncated mean is {sum(subList)/len(subList)}")   
+else:
+    print("The list is empty")
