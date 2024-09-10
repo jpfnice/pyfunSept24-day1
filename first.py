09bc5ea18b1501281c7e94d74c33d7ec04a8ad6d
jpfnice <jpfnice@gmail.com>
2024-09-10 10:21:55 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,30 @@
+avalue=(12*3)/5 # avalue is a variable
+print("the value of avalue is", avalue)
+
+aValue=99
+print("the value of avalue is", avalue, aValue)
+
+avalue="Hello"
+print("the value of avalue is", avalue)
+
+name="Forestier" 
+
+firstname="Jean Philippe"
+
+print(name)
+
+nbr=5 * 3 + 12 / 3 / 34
+
+print(nbr)
+
+print("nbr", nbr)
+print('nbr', nbr)
+
+nbr=5 * 3 + 12 / 3 / \
+    34 *2
+
+print(nbr)
+
+
+
+
---
+++ ifSyntax1.py
@@ -0,0 +1,21 @@
+
+"""
+
+if condition :
+    statement 1
+    statement 2
+statement 3
+    
+"""
+
+
+nb=23
+
+if nb > 10 :
+    print(nb, "is > 10")
+    print("End of the 'action' taken when the if condition is True")
+
+# The following print statement is executed irrespective of the 
+# boolean if condition:
+    
+print("End of the script") 
